dc056f31e5a870d4158bb3cf3e03b6227434f8f0
OlegSouzdalev <oleg.souzdalev@yahoo.fr>
2019-08-06 12:56:08 +0200

    xml editing

--- apeer_ometiff_library/omexmlClass.py
+++ apeer_ometiff_library/omexmlClass.py
@@ -404,6 +404,8 @@ class OMEXML(object):
                 self.root_node, qn(self.ns['sa'], "StructuredAnnotations"))
         return self.StructuredAnnotations(node)
 
+#----------------   Subclass   -----------------------------------------------------------
+
     class Image(object):
         '''Representation of the OME/Image element'''
 
