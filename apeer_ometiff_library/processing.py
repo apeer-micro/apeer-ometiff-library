5833b25227af4f8e034c382509c2f89f91f9ca87
OlegSouzdalev <oleg.souzdalev@yahoo.fr>
2019-09-12 16:24:26 +0200

    removed unecessary block

--- apeer_ometiff_library/processing.py
+++ apeer_ometiff_library/processing.py
@@ -1,8 +1,3 @@
-import itertools as it
-import numpy as np
-import skimage
-from apeer_ometiff_library import io
-
 
 def apply_2d_trafo(trafo_2d, array_5d, inputs):
     n_t, n_z, n_c, n_x, n_y = np.shape(array_5d)
@@ -47,11 +42,3 @@ def apply_3d_trafo_rgb(trafo_3d, array_5d, inputs):
         array_out_5d[t, z, :, :, :] = result
 
     return array_out_5d
-
-if __name__ == "__main__":
-    path = ""
-    (array5d, omexml) = io.read_ometiff(path)
-
-    arrayOut5d = apply_2d_trafo(skimage.util.invert, array5d, None)
-
-    io.write_ometiff("Test.ome.tiff", arrayOut5d, omexml)
