714ff49f89fde4eea75d8f08960dc93f94be7aba
OlegSouzdalev <oleg.souzdalev@yahoo.fr>
2019-09-12 16:04:21 +0200

    pseudo code written

--- apeer_ometiff_library/processing.py
+++ apeer_ometiff_library/processing.py
@@ -1,32 +1,57 @@
 import itertools as it
 import numpy as np
+import skimage
+from apeer_ometiff_library import io
 
 
 def apply_2d_trafo(trafo_2d, array_5d, inputs):
-    array_out_5d = np.zeros_like(array_5d)
-    n_t, n_z, n_c, n_x, n_y = np.shape(array_out_5d)
+    n_t, n_z, n_c, n_x, n_y = np.shape(array_5d)
+    array_out_5d = None
+    firstIteration = True
 
     for t, z, c in it.product(range(n_t), range(n_z), range(n_c)):
-        array_out_5d[t, z, c, :, :] = trafo_2d(array_5d[t, z, c, :, :], inputs)
+        result = trafo_2d(array_5d[t, z, c, :, :], inputs)
+        if firstIteration:
+            array_out_5d = np.zeros_like(array_5d, result.dtype)
+            firstIteration = False
+        array_out_5d[t, z, c, :, :] = result
 
     return array_out_5d
 
 
 def apply_3d_trafo_zstack(trafo_3d, array_5d, inputs):
-    array_out_5d = np.zeros_like(array_5d)
-    n_t, n_z, n_c, n_x, n_y = np.shape(array_out_5d)
+    n_t, n_z, n_c, n_x, n_y = np.shape(array_5d)
+    array_out_5d = None
+    firstIteration = True
 
     for t, c in it.product(range(n_t), range(n_c)):
-        array_out_5d[t, :, c, :, :] = trafo_3d(array_5d[t, :, c, :, :], inputs)
+        result = trafo_3d(array_5d[t, :, c, :, :], inputs)
+        if firstIteration:
+            array_out_5d = np.zeros_like(array_out_5d[t, :, c, :, :], inputs)
+        firstIteration = False
+        array_out_5d[t, :, c, :, :] = result
 
     return array_out_5d
 
 
 def apply_3d_trafo_rgb(trafo_3d, array_5d, inputs):
-    array_out_5d = np.zeros_like(array_5d)
-    n_t, n_z, n_c, n_x, n_y = np.shape(array_out_5d)
+    n_t, n_z, n_c, n_x, n_y = np.shape(array_5d)
+    array_out_5d = None
+    firstIteration = True
 
     for t, z in it.product(range(n_t), range(n_z)):
-        array_out_5d[t, z, :, :, :] = trafo_3d(array_5d[t, z, :, :, :], inputs)
+        result = trafo_3d(array_5d[t, z, :, :, :], inputs)
+        if firstIteration:
+            array_out_5d = np.zeros_like(array_out_5d[t, z, :, :, :], inputs)
+        firstIteration = False
+        array_out_5d[t, z, :, :, :] = result
 
     return array_out_5d
+
+if __name__ == "__main__":
+    path = ""
+    (array5d, omexml) = io.read_ometiff(path)
+
+    arrayOut5d = apply_2d_trafo(skimage.util.invert, array5d, None)
+
+    io.write_ometiff("Test.ome.tiff", arrayOut5d, omexml)
