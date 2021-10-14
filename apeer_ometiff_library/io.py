29cbbd3f072c9cf4bf1b47e3bcaf844966c67762
Daniel Godin <daniel.godin.ext@zeiss.com>
2021-10-14 14:04:36 +0200

    updated to support tiffile older than 2020.9.30

--- apeer_ometiff_library/io.py
+++ apeer_ometiff_library/io.py
@@ -151,5 +151,5 @@ def write_ometiff(output_path, array, omexml_string = None, compression=None):
     if omexml_string is None:
         omexml_string = gen_xml(array)
         
-    tifffile.imwrite(output_path, array,  photometric = "minisblack", description=omexml_string, metadata = None, compression=compression)
+    tifffile.imwrite(output_path, array,  photometric = "minisblack", description=omexml_string, metadata = None, compress=compression)
     
