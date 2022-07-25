ca27420345625782105af826120ff88cc1a5c631
Raphaël Dias--Monteiro <raphael@matderic.as>
2022-07-25 11:55:12 +0200

    Allow newer version of numpy

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 from setuptools import setup
 
 setup(name='apeer-ometiff-library',
-      version='1.10.0',
+      version='1.10.1',
       description='Library to read and write ometiff images',
       url='https://github.com/apeer-micro/apeer-ometiff-library',
       author='apeer-micro',
       packages=setuptools.find_packages(),
-      install_requires=['numpy==1.18.5','tifffile==2020.6.3', 'imagecodecs==2020.5.30'],
+      install_requires=['numpy>=1.18.5','tifffile==2020.6.3', 'imagecodecs==2020.5.30'],
       license='MIT',
       classifiers=[
           "Programming Language :: Python :: 3",
